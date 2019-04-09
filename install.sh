ef8abf170deb91a56e6c1f47a2029cdadc7d44a3
JAMESMTL <48530205+JAMESMTL@users.noreply.github.com>
2019-04-08 22:21:06 -0400

    initital upload

---
+++ install.sh
@@ -0,0 +1,433 @@
+#!/bin/sh
+
+sourceDir=$(dirname $0)
+sourceVer=debian-v9-router
+sourceUrl=https://raw.githubusercontent.com/JAMESMTL/${sourceVer}/master
+sourceTar=https://github.com/JAMESMTL/${sourceVer}/tarball/master
+
+dirList=" \
+	/opt/router/files \
+	/opt/router/install \
+	/root/router/action \
+	/root/router/quickstart \
+"
+
+echo
+echo "##########################################################"
+echo "Checking permissions"
+echo "##########################################################"
+echo
+
+echo -n "Verifying user ... "
+if [ ${USER} != 'root' ]; then
+	echo FAILED
+	echo Not running as root, exiting.
+	echo
+	exit
+else
+	echo ok
+fi
+
+echo
+echo "##########################################################"
+echo "Enabling root ssh access"
+echo "##########################################################"
+echo
+
+if grep -qE '^PermitRootLogin yes$' /etc/ssh/sshd_config; then
+
+	echo root ssh login already enabled ... skipping
+
+else
+
+	echo -n "writting to /etc/ssh/sshd_config ... "
+
+	echo >> /etc/ssh/sshd_config
+	echo PermitRootLogin yes >> /etc/ssh/sshd_config
+
+	grep -qE '^PermitRootLogin yes$' /etc/ssh/sshd_config && echo ok || echo FAILED
+
+fi
+
+echo
+echo "##########################################################"
+echo "Restarting sshd"
+echo "##########################################################"
+echo
+
+echo -n "Restarting sshd service ... "
+service sshd restart && echo ok || echo FAILED
+
+echo
+echo "##########################################################"
+echo "Creating directories"
+echo "##########################################################"
+echo
+
+for listItem in $dirList; do
+	echo -n "creating directory ${listItem} ... "
+	[ ! -d "${listItem}" ] && mkdir -p ${listItem}
+	[ -d "${listItem}" ] && echo ok || echo FAILED
+done
+
+echo
+echo "##########################################################"
+echo "Fetching install files"
+echo "##########################################################"
+echo
+
+# Move install to /opt/router/install/
+echo -n "copying $0 /opt/router/install/ ... "
+cp $0 /opt/router/install/ && echo ok || echo FAILED
+
+useLocalSource=no
+
+# Detect if local archive exists
+if [ -f "${sourceDir}/${sourceVer}.tar.gz" ]; then
+	while true; do
+		read -p "Local archive detected, use local archive (y/n)? " yn
+		case $yn in
+			[Yy]* )
+				useLocalSource=yes
+				break;;
+			[Nn]* )
+				useLocalSource=no
+				break;;
+		esac
+	done
+	echo
+fi
+
+# Download or use local copy of archive
+if [ $useLocalSource = 'yes' ]; then
+		echo -n "copying ${sourceDir}/${sourceVer}.tar.gz -> /opt/router/install/${sourceVer}.tar.gz ... "
+		cp ${sourceDir}/${sourceVer}.tar.gz /opt/router/install/
+		[ -f "/opt/router/install/${sourceVer}.tar.gz" ] && echo ok || echo FAILED
+else
+		echo -n "fetching /opt/router/install/${sourceVer}.tar.gz ... "
+		wget -q ${sourceTar} -O /opt/router/install/${sourceVer}.tar.gz
+		[ -f "/opt/router/install/${sourceVer}.tar.gz" ] && echo ok || echo FAILED
+fi
+
+echo
+echo "##########################################################"
+echo "setting permissions"
+echo "##########################################################"
+echo
+
+echo -n "chmod 755 -R /opt/router/install/*.sh ... "
+chmod 755 -R /opt/router/install/*.sh && echo ok || echo FAILED
+
+echo
+echo "##########################################################"
+echo "Eanbling non-free repo and updating"
+echo "##########################################################"
+echo
+
+sed -i 's/stretch main$/stretch main non-free/g' /etc/apt/sources.list
+apt update
+apt upgrade -y
+
+echo
+echo "##########################################################"
+echo "Installing base and utility packages"
+echo "##########################################################"
+echo
+
+apt install -y vlan bridge-utils net-tools ppp ipset traceroute nmap conntrack ndisc6 whois dnsutils mtr iperf3 curl resolvconf sudo apt-transport-https tcpdump ethtool firmware-bnx2x miniupnpd
+
+echo
+echo "##########################################################"
+echo "Installing vmware tools"
+echo "##########################################################"
+echo
+
+while true; do
+	read -p "Install open-vm-tools (y/n)? " yn
+	case $yn in
+		[Yy]* )
+			echo
+			apt install -y open-vm-tools
+			break;;
+		[Nn]* )
+			echo Skipping ...
+			break;;
+	esac
+done
+
+echo
+echo "##########################################################"
+echo "Installing services"
+echo "##########################################################"
+echo
+
+apt install -y unbound dnsmasq inadyn openvpn wide-dhcpv6-client
+
+service dnsmasq stop
+service unbound stop
+
+echo
+echo "##########################################################"
+echo "Installing igmpproxy 0.2.1 from buster"
+echo "##########################################################"
+echo
+
+useLocalCopy=no
+useLocalPath=""
+
+[ -f "/opt/router/install/igmpproxy_0.2.1-1_amd64.deb" ] && useLocalPath="/opt/router/install/igmpproxy_0.2.1-1_amd64.deb"
+[ -f "${sourceDir}/igmpproxy_0.2.1-1_amd64.deb" ] && useLocalPath="${sourceDir}/igmpproxy_0.2.1-1_amd64.deb"
+
+
+# Detect if local version exists
+if [ ! -z "$useLocalPath" ]; then
+	while true; do
+		read -p "Local copy of found, use local copy (y/n)? " yn
+		case $yn in
+			[Yy]* )
+				useLocalCopy=yes
+				if [ $(dirname $useLocalPath) != "/opt/router/install" ]; then
+					cp $useLocalPath /opt/router/install
+				fi
+				break;;
+			[Nn]* )
+				useLocalCopy=no
+				break;;
+		esac
+	done
+	echo
+fi
+
+[ "$useLocalCopy"  = 'no' ] && wget -q -O /opt/router/install/igmpproxy_0.2.1-1_amd64.deb http://ftp.us.debian.org/debian/pool/main/i/igmpproxy/igmpproxy_0.2.1-1_amd64.deb
+dpkg -i /opt/router/install/igmpproxy_0.2.1-1_amd64.deb
+
+echo
+echo "##########################################################"
+echo "Installing miniupnpd 2.1 from buster"
+echo "##########################################################"
+echo
+
+useLocalCopy=no
+useLocalPath=""
+
+[ -f "/opt/router/install/miniupnpd_2.1-5_amd64.deb" ] && useLocalPath="/opt/router/install/miniupnpd_2.1-5_amd64.deb"
+[ -f "${sourceDir}/miniupnpd_2.1-5_amd64.deb" ] && useLocalPath="${sourceDir}/miniupnpd_2.1-5_amd64.deb"
+
+
+# Detect if local version exists
+if [ ! -z "$useLocalPath" ]; then
+	while true; do
+		read -p "Local copy of found, use local copy (y/n)? " yn
+		case $yn in
+			[Yy]* )
+				useLocalCopy=yes
+				if [ $(dirname $useLocalPath) != "/opt/router/install" ]; then
+					cp $useLocalPath /opt/router/install
+				fi
+				break;;
+			[Nn]* )
+				useLocalCopy=no
+				break;;
+		esac
+	done
+	echo
+fi
+
+[ "$useLocalCopy"  = 'no' ] && wget -q -O /opt/router/install/miniupnpd_2.1-5_amd64.deb http://ftp.us.debian.org/debian/pool/main/m/miniupnpd/miniupnpd_2.1-5_amd64.deb
+dpkg-deb -x /opt/router/install/miniupnpd_2.1-5_amd64.deb /tmp/miniupnpd
+
+cp /tmp/miniupnpd/usr/sbin/miniupnpd /usr/sbin
+
+echo "cleaning up the mess ..."
+echo -n "rm /etc/init.d/miniupnpd ... "
+rm /etc/init.d/miniupnpd && echo ok || echo FAILED
+echo -n "rm /etc/miniupnpd/* ... "
+rm /etc/miniupnpd/* && echo ok || echo FAILED
+
+echo
+echo "##########################################################"
+echo "Extracting archive to /opt/router"
+echo "##########################################################"
+echo
+
+# Get file list from archive
+fileList=$(tar -tvf /opt/router/install/${sourceVer}.tar.gz | awk '{print $6}' | grep -oE '^.*/files/.*' | sed "s/.*-${sourceVer}-.*\/files\///g" | grep -vE '/$')
+
+# Extract archive
+tar -C /opt/router/files/ -xvf /opt/router/install/${sourceVer}.tar.gz --strip=2 | sed "s/.*-${sourceVer}-.*\/files\///g" | grep -vE '/$'
+
+
+echo
+echo "##########################################################"
+echo "backup of original files that will be overwritten"
+echo "##########################################################"
+echo
+
+if [ ! -d "/opt/router/files.bak/" ]; then
+	for listItem in $fileList; do
+		if [ -f "/${listItem}" ]; then
+			echo -n "backing up /${listItem} ... "
+			[ ! -d "/opt/router/files.bak/$(dirname $listItem)" ] && mkdir -p "/opt/router/files.bak/$(dirname $listItem)"
+			cp /${listItem} /opt/router/files.bak/$(dirname $listItem)
+			[ -f "/opt/router/files.bak/${listItem}" ] && echo ok || echo FAILED
+		fi
+	done
+else
+	echo Backup of original files exists ... skipping
+fi
+
+echo
+echo "##########################################################"
+echo "copying files"
+echo "##########################################################"
+echo
+
+for listItem in $fileList; do
+	echo -n "copying /opt/router/files/${listItem} -> /${listItem} ... "
+	[ ! -d "/$(dirname $listItem)" ] && mkdir -p /$(dirname $listItem)
+	cp /opt/router/files/${listItem} /${listItem}
+	[ -f "/${listItem}" ] && echo ok || echo FAILED
+done
+
+echo
+echo "######################################"
+echo "creating symlinks"
+echo "######################################"
+echo
+
+echo -n "creating /root/router/quickstart/network_interfaces ... "
+ln -sf /etc/network/interfaces.router /root/router/quickstart/network_interfaces && echo ok || echo FAILED
+echo -n "creating /root/router/quickstart/dhcp_base ... "
+ln -sf /opt/router/dnsmasq/dnsmasq.conf.router /root/router/quickstart/dhcp_base && echo ok || echo FAILED
+echo -n "creating /root/router/quickstart/dhcp_hosts ... "
+ln -sf /opt/router/dnsmasq/dnsmasq.hosts /root/router/quickstart/dhcp_hosts && echo ok || echo FAILED
+echo -n "creating /root/router/quickstart/pppoe ... "
+ln -sf /etc/ppp/peers/pppoe.conf /root/router/quickstart/pppoe && echo ok || echo FAILED
+echo -n "creating /root/router/quickstart/network_interfaces_map ... "
+ln -sf /etc/udev/rules.d/10-network.rules /root/router/quickstart/network_interfaces_map && echo ok || echo FAILED
+echo -n "creating /root/router/quickstart/static_split_dns ... "
+ln -sf /opt/router/unbound/unbound.static /root/router/quickstart/static_split_dns && echo ok || echo FAILED
+echo -n "creating /root/router/quickstart/he_tunnel_ddns ... "
+ln -sf /opt/router/scripts/ddns/ddns-ipv4-he-tunnel /root/router/quickstart/he_tunnel_ddns && echo ok || echo FAILED
+echo -n "creating /root/router/quickstart/wan_up ... "
+ln -sf /opt/router/scripts/system/wan-up /root/router/quickstart/wan_up && echo ok || echo FAILED
+echo -n "creating /root/router/quickstart/forwarding_v4.set ... "
+ln -sf /opt/router/nftables/port_forwarding_v4.set /root/router/quickstart/forwarding_v4.set && echo ok || echo FAILED
+echo -n "creating /root/router/quickstart/forwarding_v6.set ... "
+ln -sf /opt/router/nftables/port_forwarding_v6.set /root/router/quickstart/forwarding_v6.set && echo ok || echo FAILED
+
+echo -n "creating /root/router/action/reload-forwarding.sh ... "
+ln -sf /opt/router/scripts/system/load-forwarding-rules /root/router/action/reload-forwarding.sh && echo ok || echo FAILED
+echo -n "creating /root/router/action/activate.sh ... "
+ln -sf /opt/router/scripts/system/activate /root/router/action/activate.sh && echo ok || echo FAILED
+echo -n "creating /root/router/action/backup.sh ... "
+ln -sf /opt/router/scripts/system/backup /root/router/action/backup.sh && echo ok || echo FAILED
+echo -n "creating /root/router/action/ssh-lock.sh ... "
+ln -sf /opt/router/scripts/system/ssh-lock /root/router/action/ssh-lock.sh && echo ok || echo FAILED
+echo -n "creating /root/router/action/ssh-unlock.sh ... "
+ln -sf /opt/router/scripts/system/ssh-unlock /root/router/action/ssh-unlock.sh && echo ok || echo FAILED
+echo -n "creating /root/router/action/ssh-reset.sh ... "
+ln -sf /opt/router/scripts/system/ssh-reset /root/router/action/ssh-reset.sh && echo ok || echo FAILED
+
+echo
+echo "######################################"
+echo "Reloading daemon configs"
+echo "######################################"
+echo
+
+echo -n "disabling autostart of miniupnpd ... "
+update-rc.d miniupnpd disable && echo "ok" || echo "FAILED"
+echo -n "disabling autostart of wide-dhcpv6-client ... "
+update-rc.d wide-dhcpv6-client disable && echo "ok" || echo "FAILED"
+echo -n "reloading daemon configs ... "
+systemctl daemon-reload  && echo "ok" || echo "FAILED"
+
+echo -n "unmasking miniupnpd ... "
+systemctl unmask miniupnpd && echo "ok" || echo "FAILED"
+echo "enabling miniupnpd ... "
+systemctl enable miniupnpd
+
+echo
+echo "######################################"
+echo "creating new ssh keys"
+echo "######################################"
+echo
+
+[ -d "/root/.ssh" ] && rm /root/.ssh/*
+echo -n "generating ssh keys ... "
+ssh-keygen -f /root/.ssh/${USER}@$(cat /etc/hostname) -t rsa -N '' -q && echo "ok" || echo "FAILED"
+echo -n "replacing authorized keys ... "
+cp /root/.ssh/${USER}@$(cat /etc/hostname).pub /root/.ssh/authorized_keys && echo "ok" || echo "FAILED"
+
+echo
+echo "##########################################################"
+echo "Install backup of locally modified files"
+echo "##########################################################"
+echo
+
+useLocalCopy=no
+useLocalPath=""
+
+[ -f "/opt/router/install/${sourceVer}-local.tar.gz" ] && useLocalPath="/opt/router/install/${sourceVer}-local.tar.gz"
+[ -f "${sourceDir}/${sourceVer}-local.tar.gz" ] && useLocalPath="${sourceDir}/${sourceVer}-local.tar.gz"
+
+# Detect if archive.local exists
+if [ ! -z "$useLocalPath" ]; then
+	while true; do
+		read -p "Backup of locally modified files detected, use backup (y/n)? " yn
+		case $yn in
+			[Yy]* )
+				useLocalCopy=yes
+				break;;
+			[Nn]* )
+				useLocalCopy=no
+				echo
+				echo skipping ...
+				break;;
+		esac
+	done
+else
+	echo "${sourceVer}-local.tar.gz not found ... skipping"
+fi
+
+# Extract local backup
+if [ $useLocalCopy = 'yes' ]; then
+	
+	if [ $(dirname $useLocalPath) != "/opt/router/install" ]; then
+		echo
+		echo -n "copying $useLocalPath -> /opt/router/install/${sourceVer}-local.tar.gz ... "
+		cp $useLocalPath /opt/router/install && echo ok || echo FAILED					
+	fi
+
+	echo "Extracting files ..."
+	echo
+	tar -C / -xvf /opt/router/install/${sourceVer}-local.tar.gz
+fi
+
+echo
+echo "##########################################################"
+echo "setting permissions"
+echo "##########################################################"
+echo
+
+echo -n "chmod 755 /etc/ppp/ip-down.local ... "
+chmod 755 /etc/ppp/ip-down.local && echo ok || echo FAILED
+echo -n "chmod 755 /etc/ppp/ip-up.local ... "
+chmod 755 /etc/ppp/ip-up.local && echo ok || echo FAILED
+echo -n "chmod 755 -R /opt/router/scripts ... "
+chmod 755 -R /opt/router/scripts && echo ok || echo FAILED
+
+
+echo
+echo "######################################"
+echo "Finished"
+echo "######################################"
+echo
+echo "Please edit the files linked in the ~/router/quickstart directory"
+echo "then run ~/router/action/activate.sh"
+echo
+echo "After running the activation script, the router WILL SHUT DOWN"
+echo
+echo "The router will be fully active the next time you boot."
+echo "Make sure the original router is shutdown before booting."
+echo
